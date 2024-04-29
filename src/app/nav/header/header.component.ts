a828fda21219bfe792aba258fd9a985c3e21e627
Emile0001 <boteseh@gmail.com>
2024-04-29 22:07:08 +0200

    sidenav

--- src/app/nav/header/header.component.ts
+++ src/app/nav/header/header.component.ts
@@ -1,13 +1,21 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { ThemeService } from '../../services/theme/theme.service';
 
+import { SideNavComponent } from '../side-nav/side-nav.component';
+
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, SideNavComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
@@ -17,4 +25,12 @@ export class HeaderComponent {
   toggleTheme() {
     this.themeService.updateTheme();
   }
+
+  @Output() toggleSideNavEvent = new EventEmitter<{
+    toggleSideNavState: string;
+  }>();
+
+  //   onToggleSideNav(toggleSideNavState: string) {
+  //     toggleSideNavState === 'opened' ? 'closed' : 'opened';
+  //   }
 }
