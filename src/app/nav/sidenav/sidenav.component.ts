92935b041b399d11d50e4457c8f67b54f7f21f2a
Emile0001 <boteseh@gmail.com>
2024-06-19 11:27:14 +0200

    nav update

--- src/app/nav/sidenav/sidenav.component.ts
+++ src/app/nav/sidenav/sidenav.component.ts
@@ -1,12 +1,17 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, inject } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSidenav } from '@angular/material/sidenav';
 import { SidenavToggleService } from '../../services/sidenavToggle/sidenav-toggle.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [MatSidenavModule],
+  imports: [MatSidenavModule, AsyncPipe, MatListModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss',
 })
@@ -17,4 +22,11 @@ export class SidenavComponent {
   ngAfterViewInit() {
     this.sidenavToggleService.setSidenav(this.sidenav);
   }
+  private breakpointObserver = inject(BreakpointObserver);
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
 }
